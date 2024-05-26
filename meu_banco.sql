8f4f826c3410657f68694a9a04868f7e120ee7a4
GuiEnrik Note <gh.souza@hotmail.com>
2024-05-26 20:36:00 -0300

    Comentando o código

--- meu_banco.sql
+++ meu_banco.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS cliente (
 
 -- Insere alguns dados de exemplo na tabela
 INSERT INTO cliente (cliente_cpf, nome, endereco, telefone) VALUES
-('12345678901', 'João Silva', 'Rua A, 123', '11999999999'),
+('12345678901', 'Ana Silva', 'Rua A, 123', '11999999999'),
 ('23456789012', 'Maria Oliveira', 'Rua B, 456', '11888888888'),
 ('34567890123', 'Carlos Pereira', 'Rua C, 789', '11777777777');
